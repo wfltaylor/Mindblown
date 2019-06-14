b625ceb35d093e7f38d1031af8f4a727ff0be02b
wfltaylor <wfltaylor@gmail.com>
2019-06-14 19:15:56 +1000

    Initial Commit

---
+++ Mindblown/MindblownView.h
@@ -0,0 +1,13 @@
+//
+//  MindblownView.h
+//  Mindblown
+//
+//  Created by William Taylor on 14/6/19.
+//  Copyright © 2019 William Taylor. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MindblownView : ScreenSaverView
+
+@end
